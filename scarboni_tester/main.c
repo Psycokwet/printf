4ee4f8c8c6e403bef4dd021300ec3ff668b82d23
scarboni <socarboni@gmail.com>
2020-05-08 18:56:48 +0200

    Tous les makefiles sont fonctionnel, la libft est utilisable, ainsi que le mock de printf

---
+++ scarboni_tester/main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/08 18:24:22 by scarboni          #+#    #+#             */
+/*   Updated: 2020/05/08 18:39:35 by scarboni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "../ft_printf/libft/libft.h"
+#include "../ft_printf/ft_printf.h"
+
+int main(void)
+{
+    printf("Ah que coucou bob ");
+    ft_printf();
+    printf("%d\n", ft_isalpha('a'));
+}
---
+++ scarboni_tester/main_bonus.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/08 18:24:22 by scarboni          #+#    #+#             */
+/*   Updated: 2020/05/08 18:39:48 by scarboni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "../ft_printf/libft/libft.h"
+#include "../ft_printf/ft_printf.h"
+
+
+int main(void)
+{
+    printf("Ah que coucou bob ");
+    ft_printf();
+    printf("%d\n", ft_isalpha('a'));
+}
