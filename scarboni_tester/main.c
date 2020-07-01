4d016bba1191ba4ec80d9a4bf8b4dd125d733b1c
Psycokwet <socarboni@gmail.com>
2020-07-01 15:29:07 +0200

    Correcting test for validating the recuperation of *.* values as well of x.x values

--- scarboni_tester/main.c
+++ scarboni_tester/main.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/08 18:24:22 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/01 11:17:01 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/01 15:27:29 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main(int argc, const char * argv[])
     (void)argc;
     (void)argv;
     char * str2 = "hello";
-    char * str = "Ah que coucou bob %X%s Je suis là!%p ahaha%%sdfghjkl%s%i%u\n";
-    printf("printf returns %d\n", printf(str, 42, NULL, NULL, str2, 36, -2));
-    printf("ft_printf returns %d\n", ft_printf(str, 42, NULL, NULL, str2, 36, -2));
+    char * str = "Ah que coucou bob %X%*.*s Je suis là!%p ahaha%%sdfghjkl%s%i%u\n";
+    printf("printf returns %d\n", printf(str, 42, 4, 5, NULL, NULL, str2, 36, -2));
+    printf("ft_printf returns %d\n", ft_printf(str, 42, 4, 5, NULL, NULL, str2, 36, -2));
 }
