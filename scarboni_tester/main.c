bd02b4ae4751bbe34f1a17060ca3d873a9f6a649
Psycokwet <socarboni@gmail.com>
2020-07-03 18:37:44 +0200

    commit for test on guac, nothing serious

--- scarboni_tester/main.c
+++ scarboni_tester/main.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/08 18:24:22 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/02 16:56:17 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/03 18:37:16 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int main(int argc, const char * argv[])
     (void)argv;
     char * str2 = "hello";
     char * str3 = "";
-    char * str = "Ah que coucou bob %d%X%*.*s-Je suis là!%p ahaha%%sdfghjkl%-05.2s5.2%i%u\n";
-    printf("printf returns %d\n", printf(str, 1337, 42, 4, 2, str3, NULL, str2, 36, -2));
-    printf("ft_printf returns %d\n", ft_printf(str, 1337, 42, 4, 2, str3, NULL, str2, 36, -2));
+    char * str = "Ah que coucou bob %s%d%X%*.*s-Je suis là!%p ahaha%%sdfghjkl%-05.2s5.2%i%u\n";
+    printf("printf returns %d\n", printf(str, NULL, 1337, 42, 4, 2, str3, NULL, str2, 36, -2));
+    printf("ft_printf returns %d\n", ft_printf(str, NULL, 1337, 42, 4, 2, str3, NULL, str2, 36, -2));
 }   
