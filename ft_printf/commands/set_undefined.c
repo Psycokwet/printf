78904623fb2aaa1d2535ac7cde793b9123a449ba
Psycokwet <socarboni@gmail.com>
2020-07-07 16:06:32 +0200

    pushing for test on guac

--- ft_printf/commands/set_undefined.c
+++ ft_printf/commands/set_undefined.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/30 11:39:30 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/07 15:36:22 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/07 16:06:11 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,14 @@
 #ifndef LINUX
 
 int set_undefined(const char *code, t_data *datas){
-    printf("\nUNDEFINED BEHAVIOR [%s][%d]\n", code, datas->cursor);
     (void)code;
     (void)datas;
-    //datas->cursor++;
     return EXIT_CODE_END_FOUND;
 }
 
 #else
 
 int set_undefined(const char *code, t_data *datas){
-    //printf("\nUNDEFINED BEHAVIOR [%s]\n", code);
     (void)code;
     int ret = convert_percent(datas);
     if (ret < EXIT_SUCCESS)
