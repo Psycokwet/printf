c2e7d357eb6cfddb08b839c8540f18ccd6048a21
Psycokwet <socarboni@gmail.com>
2020-07-07 10:44:23 +0200

    pushing for test on guac

--- ft_printf/prepare_flags/prepare_flag_less.c
+++ ft_printf/prepare_flags/prepare_flag_less.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/01 08:26:48 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/07 10:39:18 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/07 10:44:13 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,7 @@
 //override a given 0
 void prepare_flag_less(t_data *datas)
 {
-    if (!(datas->active_flags & FT_PF_FLAG_PRECISION))
-        datas->active_flags |= FT_PF_FLAG_LESS;
-    else
+    if (datas->active_flags & FT_PF_FLAG_PRECISION)
         datas->unothorized_flags -= FT_PF_FLAG_PRECISION;
+    datas->active_flags |= FT_PF_FLAG_LESS;
 }
