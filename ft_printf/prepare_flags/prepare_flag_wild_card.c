15a68b380dc75aa24d5d303545a6c04c1e094fd8
Psycokwet <socarboni@gmail.com>
2020-07-07 17:39:17 +0200

    pushing for test on guac

--- ft_printf/prepare_flags/prepare_flag_wild_card.c
+++ ft_printf/prepare_flags/prepare_flag_wild_card.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/01 08:26:48 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/07 16:57:39 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/07 17:37:47 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,23 @@ void prepare_flag_wild_card(t_data *datas)
     if (datas->active_flags & FT_PF_FLAG_PRECISION && value < 0){
         if (datas->active_flags & FT_PF_FLAG_ZERO)
         {
-            datas->unauthorized_flags |= FT_PF_FLAG_PRECISION;
-            //datas->precision = datas->field_width;
-    set_precision(datas, datas->field_width, "prepare_flag_wild_card");
+            if (datas->active_flags & FT_PF_FLAG_LESS)
+            {
+                datas->unauthorized_flags |= FT_PF_FLAG_PRECISION;
+                //datas->precision = datas->field_width;
+                set_precision(datas, 1, "prepare_flag_wild_card");
+            }
+            else
+            {
+                datas->unauthorized_flags |= FT_PF_FLAG_PRECISION;
+                //datas->precision = datas->field_width;
+                set_precision(datas, datas->field_width, "prepare_flag_wild_card");
+            }
         }
         else
         {
             datas->unauthorized_flags |= FT_PF_FLAG_PRECISION;
-    set_precision(datas, 1, "prepare_flag_wild_card");
+            set_precision(datas, 1, "prepare_flag_wild_card");
             //datas->precision = 1;
         }
         //printf("PREPARE WILDCARD\n");
