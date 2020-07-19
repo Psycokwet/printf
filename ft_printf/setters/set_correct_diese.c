f664deb4ba621e97d485c3aedbfc49c61da9df64
Psycokwet <socarboni@gmail.com>
2020-07-19 11:24:24 +0200

    test guac

--- ft_printf/setters/set_correct_diese.c
+++ ft_printf/setters/set_correct_diese.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 08:00:02 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/19 11:23:00 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/19 11:24:13 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void set_correct_diese(t_data *datas)
 {
-    printf("ICI ??? [%u] [%d]\n", datas->value_ui, datas->active_flags & FT_PF_FLAG_DIESE? 1: 0);
+    printf("ICI ??? [%u] [%d] :::: ", datas->value_ui, datas->active_flags & FT_PF_FLAG_DIESE? 1: 0);
     fflush(stdout);
     if(datas->value_ui == 0 && datas->active_flags & FT_PF_FLAG_DIESE){
-        printf("ICI OUI\n");
+        printf("ICI OUI");
         fflush(stdout);
         datas->active_flags -= FT_PF_FLAG_DIESE;
     }
