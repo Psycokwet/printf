6edb81b69c03c169fd1a72ce01987e0e1824f70b
Psycokwet <socarboni@gmail.com>
2020-07-21 21:43:52 +0200

    test guac

--- ft_printf/writers/write_padding_d.c
+++ ft_printf/writers/write_padding_d.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 08:00:02 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/05 18:45:51 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/21 21:43:42 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 
 int write_padding_d(t_data *datas)
 {
+    printf("IN PADDING\n");
     return (write_padding(datas, ' ', datas->field_width));
 }
