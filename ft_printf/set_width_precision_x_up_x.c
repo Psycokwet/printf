86850d018e580e68d75ed27be910e13b378c47da
Psycokwet <socarboni@gmail.com>
2020-07-22 14:10:55 +0200

    test guac

--- ft_printf/set_width_precision_x_up_x.c
+++ ft_printf/set_width_precision_x_up_x.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 11:44:25 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/22 14:09:05 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/22 14:10:51 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void set_width_precision_x_up_x(t_data *datas)
     if(datas->active_flags & FT_PF_NEG_PRECISION 
         && datas->active_flags & FT_PF_FLAG_FIELD_WIDTH 
         && datas->active_flags & FT_PF_FLAG_ZERO 
-        && !(datas->active_flags & FT_PF_FLAG_DIESE) 
+        //&& !(datas->active_flags & FT_PF_FLAG_DIESE) 
         && !(datas->active_flags & FT_PF_FLAG_LESS)){
         datas->precision = datas->field_width;
         datas->active_flags -= FT_PF_FLAG_FIELD_WIDTH;
