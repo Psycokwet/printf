16d246d59e4c33e2084eb9725bf101e1956204ae
Psycokwet <socarboni@gmail.com>
2020-07-22 16:04:06 +0200

    test guac

--- ft_printf/write_diese_x_up_x.c
+++ ft_printf/write_diese_x_up_x.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 08:00:02 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/22 08:44:32 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/22 16:03:48 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 int write_diese_x_up_x(t_data *datas, char *diese)
 {
-    return (write_str(datas->fd, diese, 2));
+    if(!(datas->unauthorized_flags & FT_PF_FLAG_DIESE))
+        return (write_str(datas->fd, diese, 2));
+    return (EXIT_SUCCESS);
 }
--- ft_printf/writers/write_diese_up_x.c
+++ ft_printf/writers/write_diese_up_x.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 08:00:02 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/22 16:01:08 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/22 16:04:02 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,5 @@
 
 int write_diese_up_x(t_data *datas)
 {
-    if(!(datas->unauthorized_flags & FT_PF_FLAG_DIESE))
-        return (write_diese_x_up_x(datas, "0X"));
-    return (EXIT_SUCCESS);
+    return (write_diese_x_up_x(datas, "0X"));
 }
