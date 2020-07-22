a887c6f776e146e6226d78f8cb285fe5ae1a4888
Psycokwet <socarboni@gmail.com>
2020-07-22 18:27:23 +0200

    test guac

--- ft_printf/set_width_precision_x_up_x.c
+++ ft_printf/set_width_precision_x_up_x.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 11:44:25 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/22 18:25:53 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/22 18:27:19 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,10 +54,10 @@ void set_width_precision_x_up_x(t_data *datas)
         datas->active_flags -= FT_PF_FLAG_FIELD_WIDTH;
         datas->active_flags |= FT_PF_FLAG_PRECISION;
     }
-    printf("after NEGP:%d::NEGW:%d::LESS:%d::DIESE:%d::ZERO:%d::\n", 
-            datas->active_flags & FT_PF_NEG_PRECISION? 1:0,
-            !(datas->active_flags & FT_PF_NEG_FIELD_WIDTH )?1:0,
-             !(datas->active_flags & FT_PF_FLAG_LESS)?1:0,
-             datas->active_flags & FT_PF_FLAG_DIESE?1:0,
-             datas->active_flags & FT_PF_FLAG_ZERO ? 1:0);
+    // printf("after NEGP:%d::NEGW:%d::LESS:%d::DIESE:%d::ZERO:%d::\n", 
+    //         datas->active_flags & FT_PF_NEG_PRECISION? 1:0,
+    //         !(datas->active_flags & FT_PF_NEG_FIELD_WIDTH )?1:0,
+    //          !(datas->active_flags & FT_PF_FLAG_LESS)?1:0,
+    //          datas->active_flags & FT_PF_FLAG_DIESE?1:0,
+    //          datas->active_flags & FT_PF_FLAG_ZERO ? 1:0);
 }
