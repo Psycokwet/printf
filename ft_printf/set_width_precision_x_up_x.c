e34540d12b13116c0a9e8ab08e852d90ea7e6ca6
Psycokwet <socarboni@gmail.com>
2020-07-22 08:40:36 +0200

    test guac

--- ft_printf/set_width_precision_x_up_x.c
+++ ft_printf/set_width_precision_x_up_x.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 11:44:25 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/22 08:36:52 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/22 08:40:32 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,12 @@ void set_width_precision_x_up_x(t_data *datas)
     else
         datas->precision = 0;
     datas->field_width -= datas->len + datas->precision;
-    if(datas->active_flags & FT_PF_NEG_PRECISION && datas->active_flags & FT_PF_FLAG_FIELD_WIDTH && !(datas->active_flags & FT_PF_FLAG_DIESE) && !(datas->active_flags & FT_PF_FLAG_LESS)){
-        datas->precision = datas->field_width;
-        datas->active_flags -= FT_PF_FLAG_FIELD_WIDTH;
-        datas->active_flags |= FT_PF_FLAG_PRECISION;
-    }
+    // if(datas->active_flags & FT_PF_NEG_PRECISION 
+    //     && datas->active_flags & FT_PF_FLAG_FIELD_WIDTH 
+    //     && !(datas->active_flags & FT_PF_FLAG_DIESE) 
+    //     && !(datas->active_flags & FT_PF_FLAG_LESS)){
+    //     datas->precision = datas->field_width;
+    //     datas->active_flags -= FT_PF_FLAG_FIELD_WIDTH;
+    //     datas->active_flags |= FT_PF_FLAG_PRECISION;
+    // }
 }
