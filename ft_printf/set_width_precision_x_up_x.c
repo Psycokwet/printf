4f924a8cfe6cde8ed8f7313addd4fc1226313a76
Psycokwet <socarboni@gmail.com>
2020-07-22 17:29:14 +0200

    test guac

--- ft_printf/set_width_precision_x_up_x.c
+++ ft_printf/set_width_precision_x_up_x.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 11:44:25 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/22 17:24:15 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/22 17:28:57 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,12 @@ void set_width_precision_x_up_x(t_data *datas)
             && !(datas->active_flags & FT_PF_FLAG_LESS)
             && datas->unauthorized_flags & FT_PF_FLAG_DIESE
             && datas->active_flags & FT_PF_FLAG_ZERO 
+        ) || (
+            datas->active_flags & FT_PF_NEG_PRECISION 
+            && !(datas->active_flags & FT_PF_NEG_FIELD_WIDTH )
+            && !(datas->active_flags & FT_PF_FLAG_LESS)
+            && datas->active_flags & FT_PF_FLAG_DIESE
+            && datas->active_flags & FT_PF_FLAG_ZERO 
         )
     ){
         set_precision(datas, datas->field_width, "set_width_precision_x_up_x3");
@@ -46,9 +52,9 @@ void set_width_precision_x_up_x(t_data *datas)
         datas->active_flags -= FT_PF_FLAG_FIELD_WIDTH;
         datas->active_flags |= FT_PF_FLAG_PRECISION;
     }
-    printf("after NEGP:%d::NEGW:%d::LESS:%d::DIESE:%d::ZERO:%d::\n", datas->active_flags & FT_PF_NEG_PRECISION ? 1:0,
-            !(datas->active_flags & FT_PF_NEG_FIELD_WIDTH ) ? 1:0,
-             !(datas->active_flags & FT_PF_FLAG_LESS) ? 1:0,
-             datas->unauthorized_flags & FT_PF_FLAG_DIESE ? 1:0,
-             datas->active_flags & FT_PF_FLAG_ZERO  ? 1:0);
+    // printf("after NEGP:%d::NEGW:%d::LESS:%d::DIESE:%d::ZERO:%d::\n", datas->active_flags & FT_PF_NEG_PRECISION ? 1:0,
+    //         !(datas->active_flags & FT_PF_NEG_FIELD_WIDTH ) ? 1:0,
+    //          !(datas->active_flags & FT_PF_FLAG_LESS) ? 1:0,
+    //          datas->unauthorized_flags & FT_PF_FLAG_DIESE ? 1:0,
+    //          datas->active_flags & FT_PF_FLAG_ZERO  ? 1:0);
 }
