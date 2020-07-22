f6b6e8d99345eae1a9b8c25eb75e64fd5e42a8b4
Psycokwet <socarboni@gmail.com>
2020-07-22 16:12:36 +0200

    test guac

--- ft_printf/set_width_precision_x_up_x.c
+++ ft_printf/set_width_precision_x_up_x.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 11:44:25 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/22 16:12:11 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/22 16:12:33 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ void set_width_precision_x_up_x(t_data *datas)
         ) || (
             datas->active_flags & FT_PF_NEG_PRECISION 
             && !(datas->active_flags & FT_PF_NEG_FIELD_WIDTH )
-            && !(datas->active_flags & FT_PF_FLAG_LESS
+            && !(datas->active_flags & FT_PF_FLAG_LESS)
             && datas->unauthorized_flags & FT_PF_FLAG_DIESE
             && datas->active_flags & FT_PF_FLAG_ZERO 
         )
