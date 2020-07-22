4d11cab06862a1b3b511348045a7081018f80a4e
Psycokwet <socarboni@gmail.com>
2020-07-22 16:06:48 +0200

    test guac

--- ft_printf/set_width_precision_x_up_x.c
+++ ft_printf/set_width_precision_x_up_x.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 11:44:25 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/22 14:42:16 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/22 16:06:41 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 void set_width_precision_x_up_x(t_data *datas)
 {
-    //printf("prevW:%d::P:%d::L:%zu::V%d::NEG%d\n", datas->field_width, datas->precision, datas->len, datas->value_ui, (datas->active_flags & FT_PF_NEG_PRECISION)?1:0);
+    printf("prevW:%d::P:%d::L:%zu::V%d::NEG%d\n", datas->field_width, datas->precision, datas->len, datas->value_ui, (datas->active_flags & FT_PF_NEG_PRECISION)?1:0);
     if(datas->value_ui == 0 && datas->precision <= 0 && !(datas->active_flags & FT_PF_NEG_PRECISION))
         datas->len = 0;
     if((size_t)datas->precision > datas->len)
@@ -27,12 +27,18 @@ void set_width_precision_x_up_x(t_data *datas)
     set_field_width(datas, datas->field_width - (datas->len + datas->precision), "set_width_precision_x_up_x");
     // datas->field_width -= datas->len + datas->precision;
     
-    if(datas->active_flags & FT_PF_NEG_PRECISION 
+    if((datas->active_flags & FT_PF_NEG_PRECISION 
         && datas->active_flags & FT_PF_FLAG_FIELD_WIDTH 
         && datas->active_flags & FT_PF_NEG_FIELD_WIDTH 
         && datas->active_flags & FT_PF_FLAG_ZERO 
         //&& !(datas->active_flags & FT_PF_FLAG_DIESE) 
         && !(datas->active_flags & FT_PF_FLAG_LESS)
+        )
+        ||
+        (
+        datas->unauthorized_flags & FT_PF_FLAG_DIESE
+        && datas->active_flags & FT_PF_FLAG_ZERO 
+        )
         )
     {
         datas->precision = datas->field_width;
