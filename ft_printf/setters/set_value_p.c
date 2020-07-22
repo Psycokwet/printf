2c70cc690b39ebad94a53e04241305f2a2c35fb9
Psycokwet <socarboni@gmail.com>
2020-07-22 18:12:25 +0200

    test guac

--- ft_printf/setters/set_value_p.c
+++ ft_printf/setters/set_value_p.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 08:00:02 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/22 18:09:54 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/22 18:12:23 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void set_value_p(t_data *datas)
 {
     datas->active_flags |= FT_PF_FLAG_DIESE;
     datas->value_p  = va_arg(datas->list, void*);
-    printf("HEllo there !\n");
+    printf("set_value_p!\n");
 }
 
 #else
@@ -28,7 +28,7 @@ void set_value_p(t_data *datas)
     datas->value_p  = va_arg(datas->list, char*);
     if (datas->value_p){
         datas->active_flags |= FT_PF_FLAG_DIESE;
-    printf("HEllo there !\n");
+    printf("set_value_p!\n");
         return;
     }
     datas->value_s = "(nil)";
