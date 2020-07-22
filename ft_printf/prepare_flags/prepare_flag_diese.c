c1a452866ec620e9d37f6ae3956912213400a138
Psycokwet <socarboni@gmail.com>
2020-07-22 17:30:12 +0200

    test guac

--- ft_printf/prepare_flags/prepare_flag_diese.c
+++ ft_printf/prepare_flags/prepare_flag_diese.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/01 08:26:48 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/22 17:26:48 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/22 17:30:09 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,4 @@
 void prepare_flag_diese(t_data *datas)
 {
     datas->active_flags |= FT_PF_FLAG_DIESE;
-
-    printf("Well, hello there\n");
 }
