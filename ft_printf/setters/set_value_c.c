f9282bf862bcb96b8d8053f225fd59d1e4beb682
Psycokwet <socarboni@gmail.com>
2020-07-23 20:43:17 +0200

    test guac

--- ft_printf/setters/set_value_c.c
+++ ft_printf/setters/set_value_c.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 08:00:02 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/03 19:03:30 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/23 20:42:58 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,8 @@
 void set_value_c(t_data *datas)
 {
     datas->value_i = va_arg(datas->list, int);
+    datas->field_width--;
+    if (datas->field_width < 0)
+        datas->field_width = 0;
+
 }
