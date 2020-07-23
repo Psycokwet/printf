8ba943261056efae7488ef3762154da109837acc
Psycokwet <socarboni@gmail.com>
2020-07-23 22:47:24 +0200

    test guac

--- ft_printf/writers/write_value_i_sign.c
+++ ft_printf/writers/write_value_i_sign.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 08:00:02 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/03 20:23:27 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/23 22:47:20 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,8 @@ int    write_value_i_sign(t_data *datas)
         sign = '-';
     else
         sign = '+';
+    set_precision(datas, datas->precision - 1, "write_value_i_sign1");
+    if(datas->precision <0)
+        set_precision(datas, 0, "write_value_i_sign2");
     return (write(datas->fd, &sign, 1));
 }
