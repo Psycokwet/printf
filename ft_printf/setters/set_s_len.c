aa03915f8f92ff0100b1c17e19b8a651f579ab46
Psycokwet <socarboni@gmail.com>
2020-07-24 14:05:10 +0200

    test guac

--- ft_printf/setters/set_s_len.c
+++ ft_printf/setters/set_s_len.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 08:00:02 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/24 11:07:51 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/24 14:04:13 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ void set_s_len(t_data *datas)
     else
     {
         set_field_width(datas, datas->field_width - datas->len, "set_s_len3");
-        if (datas->active_flags & FT_PF_NEG_PRECISION)
+        if (datas->active_flags & FT_PF_NEG_PRECISION && datas->unauthorized_flags & FT_PF_FLAG_PRECISION)
             datas->len = 0;
         // datas->field_width -= datas->len;
     }
