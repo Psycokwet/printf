ea3e31174d9a50a8651d6b4bdbcb5a4613de9c7d
Psycokwet <socarboni@gmail.com>
2020-07-27 20:18:03 +0200

    adding space management, test guac

--- ft_printf/writers/write_one_space.c
+++ ft_printf/writers/write_one_space.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 08:00:02 by scarboni          #+#    #+#             */
-/*   Updated: 2020/07/27 20:06:34 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/07/27 20:17:58 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int write_one_space_int(t_data *datas)
     static char c;
 
     c = ' ';
-    set_field_width(datas, datas->field_width - 1, "write_one_space");
+    if(!(!(datas->active_flags & FT_PF_FLAG_PRECISION) || datas->precision == 0))
+        set_field_width(datas, datas->field_width - 1, "write_one_space");
     return (write(datas->fd, &c, 1));
 }
 
