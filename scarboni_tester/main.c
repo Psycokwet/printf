6722e579a857624672e698c2d88b17e56cd1d837
Psycokwet <socarboni@gmail.com>
2020-07-27 14:27:36 +0200

    test guac

--- scarboni_tester/main.c
+++ scarboni_tester/main.c
@@ -15,8 +15,8 @@
 #include "../ft_printf/ft_printf.h"
 
 
-static const char * POSSIBLE_FLAGS = "+-#   0";
-static const int NBR_FLAGS = 8;
+static const char * POSSIBLE_FLAGS = "+-      #   0";
+static const int NBR_FLAGS = 14;
 static const char* TEST_STRING = "Hello !";
 static const int max = 6;
 static const int min = -max;
