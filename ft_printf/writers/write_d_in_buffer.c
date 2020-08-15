dbecc9b8f19ca4d3574d3e695ac2c5653bfc1c57
Psycokwet <socarboni@gmail.com>
2020-08-15 14:13:28 +0200

    norme

--- ft_printf/writers/write_d_in_buffer.c
+++ ft_printf/writers/write_d_in_buffer.c
@@ -6,11 +6,19 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 08:00:02 by scarboni          #+#    #+#             */
-/*   Updated: 2020/08/15 14:05:53 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/08/15 14:13:22 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
+
+static int flatten_value(int value)
+{
+	if(value < 0)
+		return (0);
+	else
+		return (value);
+}
 
 static void	set_width_precision_d(t_data *datas, int sign)
 {
@@ -23,8 +31,7 @@ static void	set_width_precision_d(t_data *datas, int sign)
 			&& datas->active_flags & FT_PF_FLAG_ZERO)
 		&& datas->precision < datas->field_width
 		&& (!(datas->active_flags & FT_PF_FLAG_LESS))
-		&& (!(datas->active_flags & FT_PF_FLAG_PRECISION))
-	)
+		&& (!(datas->active_flags & FT_PF_FLAG_PRECISION)))
 	{
 		datas->active_flags |= FT_PF_FLAG_PRECISION;
 		datas->precision = datas->field_width - ((datas->active_flags & FT_PF_FLAG_PLUS || sign < 0) ? 1 : 0);
@@ -32,26 +39,16 @@ static void	set_width_precision_d(t_data *datas, int sign)
 	precision_tmp = 0;
 	if (datas->active_flags & FT_PF_FLAG_PRECISION)
 	{
-		datas->precision -= datas->len;
-		if (datas->precision < 0)
-			datas->precision = 0;
+		datas->precision = flatten_value(datas->precision - datas->len);
 		precision_tmp = datas->precision;
 	}
 	if ((datas->active_flags & FT_PF_FLAG_PRECISION) && datas->field_width < precision_tmp)
 		datas->field_width = 0;
-	datas->field_width = datas->field_width - (datas->len + (-sign) + precision_tmp);
-	if (datas->field_width < 0)
-		datas->field_width = 0;
+	datas->field_width = flatten_value(datas->field_width - (datas->len + (-sign) + precision_tmp));
 	if (datas->active_flags & FT_PF_NEG_PRECISION && datas->active_flags & FT_PF_FLAG_PLUS)
-		datas->precision--;
-	if (datas->precision < 0)
-		datas->precision = 0;
+		datas->precision = flatten_value(datas->precision - 1);
 	if (datas->active_flags & FT_PF_FLAG_PLUS && sign == 0)
-	{
-		datas->field_width--;
-		if (datas->field_width < 0)
-			datas->field_width = 0;
-	}
+		datas->field_width = flatten_value(datas->field_width - 1);
 }
 
 int			write_d_in_buffer(t_data *datas)
