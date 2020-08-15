70cceaad4f19cd3033c32c1c9a04e5863aa60869
Psycokwet <socarboni@gmail.com>
2020-08-15 09:32:57 +0200

    norme

--- ft_printf/prepare_flags/set_precision_or_fieldwith.c
+++ ft_printf/prepare_flags/set_precision_or_fieldwith.c
@@ -14,27 +14,27 @@
 
 static void	set_precision_or_fieldwith_int(t_data *datas, const int value, const int sign)
 {
-    if (datas->active_flags & FT_PF_FLAG_PRECISION)
-    {
-        if (sign >= 0 && !(datas->unauthorized_flags & FT_PF_FLAG_PRECISION))
-            datas->precision = value;
-    }
-    else
-    {
-        datas->active_flags |= FT_PF_FLAG_FIELD_WIDTH;
-        datas->field_width = value;
-        if (sign < 0)
-            datas->active_flags |= FT_PF_NEG_FIELD_WIDTH;
-    }
+	if (datas->active_flags & FT_PF_FLAG_PRECISION)
+	{
+		if (sign >= 0 && !(datas->unauthorized_flags & FT_PF_FLAG_PRECISION))
+			datas->precision = value;
+	}
+	else
+	{
+		datas->active_flags |= FT_PF_FLAG_FIELD_WIDTH;
+		datas->field_width = value;
+		if (sign < 0)
+			datas->active_flags |= FT_PF_NEG_FIELD_WIDTH;
+	}
 }
 
 void		set_precision_or_fieldwith(t_data *datas, const int value)
 {
-    if (value < 0)
+	if (value < 0)
 	{
-        set_precision_or_fieldwith_int(datas, -value, -1);
-        prepare_flag_less(datas);
-    }
-    else
-        set_precision_or_fieldwith_int(datas, value, 0);
+		set_precision_or_fieldwith_int(datas, -value, -1);
+		prepare_flag_less(datas);
+	}
+	else
+		set_precision_or_fieldwith_int(datas, value, 0);
 }
