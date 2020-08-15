eca429c86954d7198c7e3d3da40eaf23496abad9
Psycokwet <socarboni@gmail.com>
2020-08-15 20:17:51 +0200

    norme

--- ft_printf/write_ui_value_in_buffer.c
+++ ft_printf/write_ui_value_in_buffer.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 11:44:34 by scarboni          #+#    #+#             */
-/*   Updated: 2020/08/15 20:16:55 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/08/15 20:17:48 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 int	write_ui_value_in_buffer(t_data *datas)
 {
-	const int len = ft_uitoa_ext_buffer(datas->value_ui,
-		datas->nbr_buffer, 10, 0);
+	int len;
 
+	len = ft_uitoa_ext_buffer(datas->value_ui,
+		datas->nbr_buffer, 10, 0);
 	if (len <= EXIT_SUCCESS)
 		return (-EXIT_FAILURE);
 	datas->len = (size_t)len;
