ac1e2fd00d96853040f04149c408f2439b339892
Psycokwet <socarboni@gmail.com>
2020-08-15 15:02:59 +0200

    norme

--- ft_printf/converts/convert_x.c
+++ ft_printf/converts/convert_x.c
@@ -6,13 +6,14 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 08:00:02 by scarboni          #+#    #+#             */
-/*   Updated: 2020/08/15 14:51:35 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/08/15 15:02:50 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-# define MAX_WRITTER_X		6
+#define MAX_WRITTER_X		6
+
 static const t_write WRITER_X[MAX_WRITTER_X] = {
 	(t_write){FT_PF_FLAG_FIELD_WIDTH | FT_PF_FLAG_LESS, FT_PF_FLAG_FIELD_WIDTH | FT_PF_FLAG_LESS, &write_padding_d},
 	(t_write){FT_PF_FLAG_WRITE, FT_PF_FLAG_WRITE, &write_nbr_buffer},
