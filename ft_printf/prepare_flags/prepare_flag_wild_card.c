1d2847e75324f9f58a830f5a9c597e3397c7f103
Psycokwet <socarboni@gmail.com>
2020-08-15 09:43:23 +0200

    norme

--- ft_printf/prepare_flags/prepare_flag_wild_card.c
+++ ft_printf/prepare_flags/prepare_flag_wild_card.c
@@ -14,24 +14,18 @@
 
 void	prepare_flag_wild_card(t_data *datas)
 {
-	const int value;
-
-	value = va_arg(datas->list, int);
+	const int value = va_arg(datas->list, int);
 	if (datas->active_flags & FT_PF_FLAG_PRECISION && value < 0)
 	{
 		if (datas->active_flags & FT_PF_FLAG_ZERO)
 		{
 			if (datas->active_flags & FT_PF_FLAG_LESS)
-			{
 				datas->precision = 1;
-			}
 			else
-			{
 				if (datas->field_width == 0)
 					datas->precision = 1;
 				else
 					datas->precision = datas->field_width;
-			}
 			datas->unauthorized_flags |= FT_PF_FLAG_PRECISION;
 		}
 		else
