659db8a501bc69214c5de512f4cccb7e3cd8b9ca
Psycokwet <socarboni@gmail.com>
2020-08-21 11:52:17 +0200

    norme, but with big change, warning do tests again

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/08 16:39:42 by scarboni          #+#    #+#             */
-/*   Updated: 2020/08/19 16:45:13 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/08/21 11:51:50 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,8 @@ int		parse_format(t_data *datas)
 	datas->padding_c = ' ';
 	while (--command_id >= 0)
 	{
-		ret = g_commands[command_id].command(datas->format_s + datas->cursor, datas);
+		ret = g_commands[command_id].command(datas->format_s + datas->cursor,
+			datas);
 		if (ret == EXIT_CODE_END_FOUND || ret <= -EXIT_FAILURE)
 			return (ret);
 		else if (ret == EXIT_CODE_FOUND)
@@ -44,12 +45,37 @@ int		parse_format(t_data *datas)
 	return (-EXIT_FAILURE);
 }
 
+int		ft_printf_int(t_data *datas)
+{
+	int		i;
+	int		ret;
+	i = 0;
+	while (datas->format_s[datas->cursor + i] != '\0')
+		if (datas->format_s[datas->cursor + i] == '%')
+		{
+			ret = write(STDOUT_FILENO, datas->format_s + datas->cursor, i);
+			if (ret != i)
+				return (-EXIT_FAILURE);
+			datas->written_count += i;
+			datas->cursor += i + 1;
+			i = 0;
+			datas->last_percent_found = datas->cursor;
+			ret = parse_format(datas);
+			if (ret <= -EXIT_FAILURE)
+				return (-EXIT_FAILURE);
+		}
+		else
+			i++;
+	ret = write(STDOUT_FILENO, datas->format_s + datas->cursor, i);
+	if (ret != i)
+		return (-EXIT_FAILURE);
+	datas->written_count += i;
+	return (EXIT_SUCCESS);
+}
 int		ft_printf(const char *format_s, ...)
 {
-	int i;
-	int r_write;
-	t_data datas;
-	int ret;
+	t_data	datas;
+	int		ret;
 
 	datas.format_s = format_s;
 	datas.cursor = 0;
@@ -57,33 +83,7 @@ int		ft_printf(const char *format_s, ...)
 	datas.precision = 1;
 	datas.fd = STDOUT_FILENO;
 	va_start(datas.list, format_s);
-	ret = -EXIT_FAILURE;
-	i = 0;
-	while (format_s[datas.cursor + i] != '\0')
-	{
-		if (format_s[datas.cursor + i] == '%')
-		{
-			r_write = write(STDOUT_FILENO, format_s + datas.cursor, i);
-			if (r_write != i)
-				return (-EXIT_FAILURE);
-			datas.written_count += i;
-			datas.cursor += i + 1;
-			i = -1;
-			datas.last_percent_found = datas.cursor;
-			ret = parse_format(&datas);
-			if (ret <= -EXIT_FAILURE)
-				break;
-		}
-		i++;
-	}
-	if (format_s[datas.cursor + i] == '\0')
-	{
-		r_write = write(STDOUT_FILENO, format_s + datas.cursor, i);
-		if (r_write != i)
-			return (-EXIT_FAILURE);
-		datas.written_count += i;
-		datas.cursor = i + 1;
-	}
+	ret = ft_printf_int(&datas);
 	va_end(datas.list);
-	return (datas.written_count);
+	return (ret == (-EXIT_FAILURE) ? ret : datas.written_count);
 }
