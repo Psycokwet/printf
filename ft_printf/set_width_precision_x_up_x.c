44b64f72d29c024763745aec0055adb17da52afb
Psycokwet <socarboni@gmail.com>
2020-08-21 14:01:01 +0200

    norme and tests ok

--- ft_printf/set_width_precision_x_up_x.c
+++ ft_printf/set_width_precision_x_up_x.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 11:44:25 by scarboni          #+#    #+#             */
-/*   Updated: 2020/08/21 13:59:12 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/08/21 14:00:56 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,12 @@ void	set_width_precision_x_up_x_int(t_data *datas)
 			&& datas->active_flags & FT_PF_NEG_FIELD_WIDTH
 			&& datas->active_flags & FT_PF_FLAG_ZERO
 			&& !(datas->active_flags & FT_PF_FLAG_LESS))
-		|| (
-			datas->active_flags & FT_PF_NEG_PRECISION
+		|| (datas->active_flags & FT_PF_NEG_PRECISION
 			&& !(datas->active_flags & FT_PF_NEG_FIELD_WIDTH)
 			&& !(datas->active_flags & FT_PF_FLAG_LESS)
 			&& datas->unauthorized_flags & FT_PF_FLAG_DIESE
 			&& datas->active_flags & FT_PF_FLAG_ZERO)
-		|| (
-			datas->active_flags & FT_PF_NEG_PRECISION
+		|| (datas->active_flags & FT_PF_NEG_PRECISION
 			&& !(datas->active_flags & FT_PF_NEG_FIELD_WIDTH)
 			&& !(datas->active_flags & FT_PF_FLAG_LESS)
 			&& datas->active_flags & FT_PF_FLAG_DIESE
