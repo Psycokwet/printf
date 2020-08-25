286e0abba1d089f8ae068549addea96ac58f3a98
Psycokwet <socarboni@gmail.com>
2020-08-25 16:13:46 +0200

    norme

--- ft_printf/setters/set_value_d.c
+++ ft_printf/setters/set_value_d.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 08:00:02 by scarboni          #+#    #+#             */
-/*   Updated: 2020/08/25 15:59:10 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/08/25 16:13:26 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	set_value_d(t_data *datas)
 {
-	if(datas->active_flags & FT_PF_FLAG_H)
+	if (datas->active_flags & FT_PF_FLAG_H)
 		datas->value_i = (short int)(va_arg(datas->list, int));
-	else if(datas->active_flags & FT_PF_FLAG_HH)
+	else if (datas->active_flags & FT_PF_FLAG_HH)
 		datas->value_i = (char)(va_arg(datas->list, int));
-	else if(datas->active_flags & FT_PF_FLAG_L)
+	else if (datas->active_flags & FT_PF_FLAG_L)
 		datas->value_i = va_arg(datas->list, long int);
-	else if(datas->active_flags & FT_PF_FLAG_LL)
+	else if (datas->active_flags & FT_PF_FLAG_LL)
 		datas->value_i = va_arg(datas->list, long long int);
 	else
 		datas->value_i = va_arg(datas->list, int);
--- ft_printf/setters/set_value_u.c
+++ ft_printf/setters/set_value_u.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/26 08:00:02 by scarboni          #+#    #+#             */
-/*   Updated: 2020/08/25 16:01:00 by scarboni         ###   ########.fr       */
+/*   Updated: 2020/08/25 16:13:13 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	set_value_ui(t_data *datas)
 {
-	if(datas->active_flags & FT_PF_FLAG_H)
+	if (datas->active_flags & FT_PF_FLAG_H)
 		datas->value_u = (unsigned short int)(va_arg(datas->list, unsigned int));
-	else if(datas->active_flags & FT_PF_FLAG_HH)
+	else if (datas->active_flags & FT_PF_FLAG_HH)
 		datas->value_u = (unsigned char)(va_arg(datas->list, unsigned int));
-	else if(datas->active_flags & FT_PF_FLAG_L)
+	else if (datas->active_flags & FT_PF_FLAG_L)
 		datas->value_u = va_arg(datas->list, unsigned long int);
-	else if(datas->active_flags & FT_PF_FLAG_LL)
+	else if (datas->active_flags & FT_PF_FLAG_LL)
 		datas->value_u = va_arg(datas->list, unsigned long long int);
 	else
 		datas->value_u = va_arg(datas->list, unsigned int);
